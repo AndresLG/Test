6e991ce470f92e92e410ce66e503261bfc4acff6
alopez <alopez@vallegrande.edu.pe>
2018-06-13 11:01:51 -0500

    Avance de mantenimiento.

--- Prac/src/java/controller/EstudianteC.java
+++ Prac/src/java/controller/EstudianteC.java
@@ -1,5 +1,43 @@
+
 package controller;
 
-public class EstudianteC {
+import dao.EstudianteDaoImpl;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import model.EstudianteM;
+
+@Named(value = "estudianteC")
+@SessionScoped
+public class EstudianteC implements Serializable {
+
+    private EstudianteM estudiante = new EstudianteM();
+    
+    public EstudianteC() {
+        
+    }
+
+    public EstudianteM getEstudiante() {
+        return estudiante;
+    }
+
+    public void setEstudiante(EstudianteM estudiante) {
+        this.estudiante = estudiante;
+    }
+    
+    public void guardar() throws Exception{
+        EstudianteDaoImpl dao;
+        
+        try {
+            dao = new EstudianteDaoImpl();
+            dao.guardar(estudiante);
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Guardado","Correctamente"));
+        } catch (Exception e) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Error","No se pudo guardar"));
+            throw e;
+        }
+    }
     
 }
