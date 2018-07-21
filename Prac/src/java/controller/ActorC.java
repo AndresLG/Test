3e3c3e012ed639f845740780eafb968c182f3d06
AndresLG <alopez@vallegrande.edu.pe>
2018-07-21 13:27:02 -0500

    Controller y Vistas en Nuevas Pruebas.

---
+++ Prac/src/java/controller/ActorC.java
@@ -0,0 +1,82 @@
+package controller;
+
+import dao.ActorD;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import lombok.Data;
+import model.ActorM;
+
+@Data
+@Named(value = "actorC")
+@SessionScoped
+public class ActorC implements Serializable {
+
+    ActorM actor = new ActorM();
+    private List<ActorM> lstactor;
+    
+    @PostConstruct
+    public void iniciar(){
+        try {
+            list();
+        } catch (Exception e) {
+        }
+    }
+    
+    public void preparActor(){
+        actor = new ActorM();
+    }
+    
+    public void add() throws Exception{
+        ActorD dao;
+        try {
+            dao = new ActorD();
+            dao.agregar(actor);
+            list();
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Agregado con Exito"));
+        } catch (Exception e) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("No se pudo Agregar"));
+            throw e;
+        }
+    }
+    
+    public void update() throws Exception{
+        ActorD dao;
+        try {
+            dao = new ActorD();
+            dao.modificar(actor);
+            list();
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Modificado con Exito"));
+        } catch (Exception e) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("No se pudo Modificar"));
+            throw e;
+        }
+    }
+    
+    public void delete() throws Exception{
+        ActorD dao;
+        try {
+            dao = new ActorD();
+            dao.eliminar(actor);
+            list();
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Eliminado con Exito"));
+        } catch (Exception e) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("No se pudo Eliminar"));
+            throw e;
+        }
+    }
+    
+    public void list() throws Exception{
+        ActorD dao;
+        try {
+            dao = new ActorD();
+            lstactor = dao.listar();
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+}
--- Prac/src/java/dao/ActorD.java
+++ Prac/src/java/dao/ActorD.java
@@ -2,6 +2,8 @@ package dao;
 
 import interfaces.ActorI;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 import model.ActorM;
 
@@ -43,7 +45,10 @@ public class ActorD extends Dao implements ActorI{
     public void eliminar(ActorM actor) throws Exception {
         try {
             this.Conectar();
-            PreparedStatement st = this.getCn().prepareStatement("");
+            PreparedStatement st = this.getCn().prepareStatement("delete from Actor where idAct=?");
+            st.setInt(1, actor.getIdAct());
+            st.setString(2, actor.getNomAct());
+            st.setString(3, actor.getApeAct());
             st.executeUpdate();
         } catch (Exception e) {
             throw e;
@@ -54,9 +59,22 @@ public class ActorD extends Dao implements ActorI{
 
     @Override
     public List<ActorM> listar() throws Exception {
+        List<ActorM> lista;
+        ResultSet rs;
         try {
             this.Conectar();
-            
+            String sql = "select * from Actor";
+            PreparedStatement st = this.getCn().prepareCall(sql);
+            rs= st.executeQuery();
+            lista = new ArrayList();
+            while(rs.next()){
+                ActorM actor= new ActorM();
+                actor.setIdAct(rs.getInt("IdAct"));
+                actor.setNomAct(rs.getString("NomAct"));
+                actor.setApeAct(rs.getString("ApeAct"));
+                lista.add(actor);
+            }
+            return lista;
         } catch (Exception e) {
             throw e;
         }finally{
